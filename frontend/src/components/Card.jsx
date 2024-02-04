716079d78fc4d8d660d5aa929f18902dc151ea23
Abdulshakourr <shakirteck4@gmail.com>
2024-02-04 17:41:32 +0300

    added get request

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,28 +1,29 @@
+import { useState } from "react";
 
-
-function Card({title, description , iscompleted, priority}) {
-  
+function Card({ title, description, complete, priority, id }) {
+  const [input, setInput] = useState({ complete });
+  console.log("card", input.complete);
   return (
-    <div className="bg-white w-full sm:max-w-[700px] my-6  px-4 py-6 rounded flex items-center flex-col sm:flex-row">
+    <div className="bg-white w-full sm:max-w-[700px] my-6  px-4 py-6 rounded flex items-center flex-col sm:flex-row shadow border-bg-gray-200">
       <div className=" sm:max-w-[306px] w-full">
-        <h1 className="font-medium text-xl mb-2">Finish user onboarding</h1>
-        <p className="text-xs text-[#AFAFAF] max-w-[306px] ">
-          We will also learn how we can retrieve the access_token from next-auth
-          session......
-        </p>
+        <h1 className="font-medium text-xl mb-2">{title} </h1>
+        <p className="text-xs text-[#AFAFAF] max-w-[306px] ">{description}</p>
       </div>
       <div className=" h-full w-full mt-4 sm:mt-0 flex sm:items-center sm:justify-around flex-col sm:flex-row">
         <div className="">
           <h2 className="text-[#AFAFAF] font-bold">Priority</h2>
-          <h3 className="font-bold low">low</h3>
+          <h3 className="font-bold low">{priority}</h3>
         </div>
         <div className="mt-4 flex gap-x-10 items-center pb-4">
           <input
-            // onChange={(e) =>
-            //   setInput({ ...input, completed: e.target.checked })
-            // }
+            onChange={(e) =>
+              setInput((prevInput) => ({
+                ...prevInput,
+                completed: e.target.checked,
+              }))
+            }
             type="checkbox"
-            // checked={input.completed}
+            checked={input.complete}
             className="border-2 rounded-full w-8 h-8 border-[#CBCBCB]"
           />
           <img src="/edit.png" alt="" />
--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,19 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import Card from "../components/Card";
+
 const API_KEY = "http://127.0.0.1:8000";
+
 function Tasks() {
-  const {token} = useSelector((state) => state.auth)
-  const {} = useQuery({
-    queryfn: async () => {
-      const {data} = await axios.get(`${API_KEY}/todos`,{
-        headers:{
-          "Accept":"application/json",
-          "Authorization": `Bearer${token}`
-        }
-      })
+  const { token } = useSelector((state) => state.auth);
+  const headers = {
+    accept: "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+  const { data } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const { data } = await axios.get(`${API_KEY}/todos/`, {
+        headers,
+      });
+      return data;
     },
   });
+
+  console.log("you", data);
 
   return (
     <div>
@@ -30,16 +38,29 @@ function Tasks() {
           </button>
         </div>
       </div>
-      {/* <div className="flex  h-96 flex-col justify-center bg-blue-500 items-center">
-        <div className="flex flex-col justify-center items-center">
-          <h2 className="text-[#AFAFAF] text-3xl pb-4 sm:text-4xl font-bold">
-            You Have No Tasks
-          </h2>
-          <button className="py-2 px-6 bg-[#AD28FF] text-white font-bold text-1xl">
-            Add Task
-          </button>
+      {data ? (
+        data.map((todo) => (
+          <Card
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            description={todo.description}
+            priority={todo.priority}
+            complete={todo.complete}
+          />
+        ))
+      ) : (
+        <div className="flex  h-96 flex-col justify-center bg-blue-500 items-center">
+          <div className="flex flex-col justify-center items-center">
+            <h2 className="text-[#AFAFAF] text-3xl pb-4 sm:text-4xl font-bold">
+              You Have No Tasks
+            </h2>
+            <button className="py-2 px-6 bg-[#AD28FF] text-white font-bold text-1xl">
+              Add Task
+            </button>
+          </div>
         </div>
-      </div> */}
+      )}
     </div>
   );
 }
